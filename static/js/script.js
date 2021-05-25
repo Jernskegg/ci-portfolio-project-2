ad7bfb3cf6c1a85fe966bb8ad7eb731cc3e45f41
Jernskegg <casper.hille@outlook.com>
2021-05-25 01:19:13 +0000

    Refactor style.display lines into a function

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,19 @@
 //hide javascript error if javascript is running
-
-
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("support").style.display = "none";
-    let howtp = document.getElementById("htp");
-
-
-
-    howtp.addEventListener("click", function () {
-        howToPlay();
-    })
-
-
     initialize();
     game();
 })
+
 /**
- * Generates random number between 1-5 and retuns it the options to compare if it's a win or lose.
+ * Generates random number between 1-5 and retuns it as a string from an array to compare if it's a win or lose.
  * 1. rock
  * 2. paper
  * 3. scissors
  * 4. lizard
  * 5. spock
  */
+
 function nonPlayerChoiceGenerator() {
     let num = Math.floor(Math.random() * 5);
     let choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
@@ -31,70 +22,58 @@ function nonPlayerChoiceGenerator() {
 }
 
 function initialize() {
+    let howtp = document.getElementById("htp");
+    howtp.addEventListener("click", function () {
+        switchDisplay("none", "block", "none", "none")
+    })
+    let iUnderstand = document.getElementById("understand")
+    iUnderstand.addEventListener("click", function () {
+        switchDisplay("block", "none", "none", "none")
+    })
+    let playAgain = document.getElementById("playAgain")
+    playAgain.addEventListener("click", function () {
+        switchDisplay("block", "none", "none", "none")
+        return;
+    })
+    let retryGame = document.getElementById("retry")
+    retryGame.addEventListener("click", function () {
+        switchDisplay("block", "none", "none", "none")
+        return;
+    })
 
 }
-
 // win game
-
 function win() {
     let oldwinscore = parseInt(document.getElementById(`wins`).innerText);
     if (oldwinscore >= 2) {
-        document.getElementById("game").style.display = "none";
-        document.getElementById("how").style.display = "none";
-        document.getElementById("win").style.display = "block";
-        document.getElementById("lose").style.display = "none";
-        reset();
+        switchDisplay("none","none","block","none")
+        resetScore();
     } else {
         document.getElementById("wins").innerText = ++oldwinscore;
     }
-
-    let playAgain = document.getElementById("playAgain")
-    playAgain.addEventListener("click", function () {
-        document.getElementById("game").style.display = "block";
-        document.getElementById("how").style.display = "none";
-        document.getElementById("win").style.display = "none";
-        document.getElementById("lose").style.display = "none";
-        return;
-    })
 }
 
 // lose game
 function lose() {
     let oldLossScore = parseInt(document.getElementById(`losses`).innerText);
     if (oldLossScore >= 2) {
-        document.getElementById("game").style.display = "none";
-        document.getElementById("how").style.display = "none";
-        document.getElementById("win").style.display = "none";
-        document.getElementById("lose").style.display = "block";
-        reset();
+        switchDisplay("none", "none", "none", "block")
+        resetScore();
     } else {
         document.getElementById("losses").innerText = ++oldLossScore;
     }
-    let retry = document.getElementById("retry")
-    retry.addEventListener("click", function () {
-        document.getElementById("game").style.display = "block";
-        document.getElementById("how").style.display = "none";
-        document.getElementById("win").style.display = "none";
-        document.getElementById("lose").style.display = "none";
-        return;
-    })
 }
-
 /**
  * runs the game area and sets the game ID to display:block and the rest to Display:none
  */
-
-
 function game() {
-    document.getElementById("game").style.display = "block";
-    document.getElementById("how").style.display = "none";
+    switchDisplay("block","none","none","none")
 
     let btnRock = document.getElementById("rock");
     let btnPaper = document.getElementById("paper");
     let btnScissors = document.getElementById("scissors");
     let btnLizard = document.getElementById("lizard");
     let btnSpock = document.getElementById("spock");
-    let result = "";
 
     btnRock.addEventListener("click", function () {
         roundGame("Rock", "lizard", "Scissors");
@@ -116,10 +95,15 @@ function game() {
         roundGame("Spock", "Paper", "Scissors");
     })
 }
-
-function roundGame(playerChoice, conflictOne, conflictTwo) {
+/**
+ *  inputs 3 values for comparison.
+ * @param {string} playerChoice, text
+ * @param {string} winningChoiceOne, 
+ * @param {string} winningChoiceTwo, 
+ */
+function roundGame(playerChoice, winningChoiceOne, winningChoiceTwo) {
     let nonPlayerChoice = nonPlayerChoiceGenerator();
-    if (nonPlayerChoice == conflictOne || nonPlayerChoice == conflictTwo) {
+    if (nonPlayerChoice == winningChoiceOne || nonPlayerChoice == winningChoiceTwo) {
         result = RoundMessage(playerChoice, nonPlayerChoice, state = 1);
     } else if (nonPlayerChoice == playerChoice) {
         result = RoundMessage(playerChoice, nonPlayerChoice, state = 2);
@@ -128,7 +112,6 @@ function roundGame(playerChoice, conflictOne, conflictTwo) {
     }
     return;
 }
-
 /**
  * function / message generator from the results made in the button functions
  * 
@@ -159,30 +142,23 @@ function RoundMessage(playerChoice, nonPlayerChoice, state) {
     return;
 }
 
-/**
- * runs the how to play area and sets the game ID to display:block and the rest to Display:none
- */
-function howToPlay() {
-    document.getElementById("game").style.display = "none";
-    document.getElementById("how").style.display = "block";
-    document.getElementById("win").style.display = "none";
-    document.getElementById("lose").style.display = "none";
-
-    let iUnderstand = document.getElementById("understand")
-
-    iUnderstand.addEventListener("click", function () {
-        document.getElementById("game").style.display = "block";
-        document.getElementById("how").style.display = "none";
-        document.getElementById("win").style.display = "none";
-        document.getElementById("lose").style.display = "none";
-        return;
-    })
-}
-
-function reset() {
+function resetScore() {
     let oldLossScore = parseInt(document.getElementById(`losses`).innerText);
     let oldwinscore = parseInt(document.getElementById(`wins`).innerText);
     document.getElementById("losses").innerText = 0;
     document.getElementById("wins").innerText = 0;
+}
+/**
+ * This changes the display mode on
+ * @param {string} block or none "displayGame" to "block" or "none" 
+ * @param {string} displayHowToPlay 
+ * @param {string} displayWinScreen 
+ * @param {string} displayLoseScreen 
+ */
 
+function switchDisplay(displayGame, displayHowToPlay, displayWinScreen, displayLoseScreen) {
+    document.getElementById("game").style.display = displayGame;
+    document.getElementById("how").style.display = displayHowToPlay;
+    document.getElementById("win").style.display = displayWinScreen;
+    document.getElementById("lose").style.display = displayLoseScreen;
 }
